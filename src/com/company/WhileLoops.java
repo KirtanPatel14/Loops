0a629ec3b50812f3e9b6f78db6e09c1d3b183d88
kirtan patel <kirtanpatel@billericak12.com>
2021-11-10 11:59:23 -0500

    nnjkgnjk vgb

--- src/com/company/WhileLoops.java
+++ src/com/company/WhileLoops.java
@@ -1,4 +1,24 @@
 package com.company;
 
 public class WhileLoops {
+
+    public int addOdds(int n){
+         int count = 1;
+         int sum = 0;
+         while(count<=n){
+             sum+=count;
+             count +=2;
+             System.out.println(sum+" "+count);
+         }
+         return sum;
+    }
+
+    public int sumDigits(int n){
+
+    }
+
+    public static void main(String[] args) {
+        WhileLoops w = new WhileLoops();
+        System.out.println(w.addOdds(9));
+    }
 }
